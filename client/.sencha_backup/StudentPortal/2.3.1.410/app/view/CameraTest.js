97fb8a8be431c709cbf770f3e023a3db1d3c5c94
jamilsj <jamilsj@timedasecurity.com>
2017-01-06 09:18:35 +0000

    test

--- client/.sencha_backup/StudentPortal/2.3.1.410/app/view/CameraTest.js
+++
@@ -1,9 +0,0 @@
-Ext.define('StudentPortal.view.CameraTest', {
-   extend: 'Ext.Panel',
-   alias: 'widget.cameratest',
-   html: '<video id="video" width="640" height="480" autoplay></video>',
-   config: {
-      fullscreen: true,
-      activeItem: 0
-   }
-});
--- client/build/temp/development/StudentPortal/sencha-compiler/app/full-page-master-bundle.js
+++
@@ -1,2 +0,0 @@
-// @tag full-page
-// @require /home/jamil/www/apex/branches/suite-4600-add-camera-functionality/client/app.js
